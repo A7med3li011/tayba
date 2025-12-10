3e40a39a4e7fb26b9fce62152aee6e7c7b8ebffb
Ahmed Ali <a7med3li0111@gmail.com>
2025-12-10 16:59:54 +0200

    handle loan info

--- src/actions/loan.actions.ts
+++ src/actions/loan.actions.ts
@@ -4,147 +4,201 @@ import axios, { AxiosError } from "axios";
 import { cookies } from "next/headers";
 
 interface LoanRequestResponse {
-    success: boolean;
-    message?: string;
-    data?: unknown;
-    errors?: Record<string, string[]>;
+  success: boolean;
+  message?: string;
+  data?: unknown;
+  errors?: Record<string, string[]>;
 }
 
-export async function requestLoan(formData: FormData): Promise<LoanRequestResponse> {
-    console.log(formData, 'formData');
-    console.log(process.env.NEXT_PUBLIC_API_URL, 'process.env.NEXT_PUBLIC_API_URL');
-    
- 
-    
-    try {
-        const cookieStore = await cookies();
+export async function requestLoan(
+  formData: FormData
+): Promise<LoanRequestResponse> {
+  console.log(formData, "formData");
+  console.log(
+    process.env.NEXT_PUBLIC_API_URL,
+    "process.env.NEXT_PUBLIC_API_URL"
+  );
 
-        
-        const sessionId = cookieStore.get("session_id")?.value;
-        const api_session = cookieStore.get("api_session")?.value;
-        // Send FormData directly to the API endpoint
-        const response = await axios.post<LoanRequestResponse>(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/loan/create`,
-            formData,
-            {
-                headers: {
-                    Cookie: `api_session=${api_session}; session_id=${sessionId}`,
-                },
-                withCredentials: true
-            },
-        );
-        console.log(response.data);
+  try {
+    const cookieStore = await cookies();
+
+    const sessionId = cookieStore.get("session_id")?.value;
+    const api_session = cookieStore.get("api_session")?.value;
+    // Send FormData directly to the API endpoint
+    const response = await axios.post<LoanRequestResponse>(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/loan/create`,
+      formData,
+      {
+        headers: {
+          Cookie: `api_session=${api_session}; session_id=${sessionId}`,
+        },
+        withCredentials: true,
+      }
+    );
+    console.log(response.data);
+    return {
+      success: true,
+      message: response.data.message || "تم إرسال طلب القرض بنجاح",
+      data: response.data,
+    };
+  } catch (error: unknown) {
+    console.error("Loan request error:", error);
+
+    if (axios.isAxiosError(error)) {
+      console.error("Error response status:", error.response?.status);
+      console.error(
+        "Error response data:",
+        JSON.stringify(error.response?.data, null, 2)
+      );
+      console.error("Error response headers:", error.response?.headers);
+
+      if (error.response?.data) {
         return {
-            success: true,
-            message: response.data.message || "تم إرسال طلب القرض بنجاح",
-            data: response.data,
+          success: false,
+          message:
+            error.response.data.message || "حدث خطأ أثناء إرسال طلب القرض",
+          errors: error.response.data.errors,
         };
-    } catch (error: unknown) {
-        console.error("Loan request error:", error);
+      }
 
-        if (axios.isAxiosError(error)) {
-            console.error("Error response status:", error.response?.status);
-            console.error("Error response data:", JSON.stringify(error.response?.data, null, 2));
-            console.error("Error response headers:", error.response?.headers);
-            
-            if (error.response?.data) {
-                return {
-                    success: false,
-                    message: error.response.data.message || "حدث خطأ أثناء إرسال طلب القرض",
-                    errors: error.response.data.errors,
-                };
-            }
-
-            return {
-                success: false,
-                message: error.message || "فشل الاتصال بالخادم",
-            };
-        }
-
-        return {
-            success: false,
-            message: "حدث خطأ غير متوقع",
-        };
+      return {
+        success: false,
+        message: error.message || "فشل الاتصال بالخادم",
+      };
     }
+
+    return {
+      success: false,
+      message: "حدث خطأ غير متوقع",
+    };
+  }
 }
 
 export async function getCountries() {
-
-    try {
-        const cookieStore = await cookies();
-        const sessionId = cookieStore.get("session_id")?.value;
-        const api_session = cookieStore.get("api_session")?.value;
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/countries`, {
-            headers: {
-                Cookie: `api_session=${api_session}; session_id=${sessionId}`,
-            },
-            withCredentials: true
-        });
-        return response.data;
-
-
-    } catch (error) {
-        console.error("Error getting countries:", error);
-        throw error;
-    }
+  try {
+    const cookieStore = await cookies();
+    const sessionId = cookieStore.get("session_id")?.value;
+    const api_session = cookieStore.get("api_session")?.value;
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/countries`,
+      {
+        headers: {
+          Cookie: `api_session=${api_session}; session_id=${sessionId}`,
+        },
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error getting countries:", error);
+    throw error;
+  }
 }
 
 export async function getLoanReasons() {
-    try {
-        const cookieStore = await cookies();
-        const sessionId = cookieStore.get("session_id")?.value;
-        const api_session = cookieStore.get("api_session")?.value;
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/loan/reasons`, {
-            headers: {
-                Cookie: `api_session=${api_session}; session_id=${sessionId}`,
-            },
-            withCredentials: true
-        });
-        return response.data;
-    } catch (error) {
-        console.error("Error getting loan reasons:", error);
-        throw error;
-    }
+  try {
+    const cookieStore = await cookies();
+    const sessionId = cookieStore.get("session_id")?.value;
+    const api_session = cookieStore.get("api_session")?.value;
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/loan/reasons`,
+      {
+        headers: {
+          Cookie: `api_session=${api_session}; session_id=${sessionId}`,
+        },
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error getting loan reasons:", error);
+    throw error;
+  }
 }
 
-export async function getUserLoansList(page: number = 1, pageLimit: number = 10) {
-    try {
-        const cookieStore = await cookies();
-        const sessionId = cookieStore.get("session_id")?.value;
-        const api_session = cookieStore.get("api_session")?.value;
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/loan/list?page=${page}&pageLimit=${pageLimit}`, {
-            headers: {
-                Cookie: `api_session=${api_session}; session_id=${sessionId}`,
-            },
-            withCredentials: true
-        });
-        return response.data;
-    } catch (error) {
-        console.error("Error getting user loans:", error);
-        throw error;
-    }
+export async function getUserLoansList(
+  page: number = 1,
+  pageLimit: number = 10
+) {
+  try {
+    const cookieStore = await cookies();
+    const sessionId = cookieStore.get("session_id")?.value;
+    const api_session = cookieStore.get("api_session")?.value;
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/loan/list?page=${page}&pageLimit=${pageLimit}`,
+      {
+        headers: {
+          Cookie: `api_session=${api_session}; session_id=${sessionId}`,
+        },
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error getting user loans:", error);
+    throw error;
+  }
 }
 
 export async function getActiveLoanDetails() {
-    try {
-        const cookieStore = await cookies();
-        const sessionId = cookieStore.get("session_id")?.value;
-        const api_session = cookieStore.get("api_session")?.value;
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/loan/active`, {
-            headers: {
-                Cookie: `api_session=${api_session}; session_id=${sessionId}`,
-            },
-            withCredentials: true
-        });
-        return {
-            success: true,
-            data: response.data,
-        };
-    } catch (error: unknown) {
-        console.error("Error getting active loan details:", error);
-        return {
-            success: false,
-            message: (error as AxiosError<{ message?: string; error?: string }>).response?.data?.message || (error as AxiosError<{ message?: string; error?: string }>).response?.data?.error || "حدث خطأ غير متوقع",
-        };
-    }
+  try {
+    const cookieStore = await cookies();
+    const sessionId = cookieStore.get("session_id")?.value;
+    const api_session = cookieStore.get("api_session")?.value;
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/loan/active`,
+      {
+        headers: {
+          Cookie: `api_session=${api_session}; session_id=${sessionId}`,
+        },
+        withCredentials: true,
+      }
+    );
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error: unknown) {
+    console.error("Error getting active loan details:", error);
+    return {
+      success: false,
+      message:
+        (error as AxiosError<{ message?: string; error?: string }>).response
+          ?.data?.message ||
+        (error as AxiosError<{ message?: string; error?: string }>).response
+          ?.data?.error ||
+        "حدث خطأ غير متوقع",
+    };
+  }
+}
+export async function getLoanDetails(id: string) {
+  try {
+    const cookieStore = await cookies();
+    const sessionId = cookieStore.get("session_id")?.value;
+    const api_session = cookieStore.get("api_session")?.value;
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/loan/${id}/details`,
+      {
+        headers: {
+          Cookie: `api_session=${api_session}; session_id=${sessionId}`,
+        },
+        withCredentials: true,
+      }
+    );
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error: unknown) {
+    console.error("Error getting active loan details:", error);
+    return {
+      success: false,
+      message:
+        (error as AxiosError<{ message?: string; error?: string }>).response
+          ?.data?.message ||
+        (error as AxiosError<{ message?: string; error?: string }>).response
+          ?.data?.error ||
+        "حدث خطأ غير متوقع",
+    };
+  }
 }
--- src/app/[locale]/user-profile/loans/page.tsx
+++ src/app/[locale]/user-profile/loans/page.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 import LoanCard from "@/components/user-profile/LoanCard";
 import { useRouter } from "next/navigation";
 import { FaCommentDollar } from "react-icons/fa";
 import { getUserLoansList } from "@/actions/loan.actions";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useTranslations, useLocale } from 'next-intl';
+import { useTranslations, useLocale } from "next-intl";
 
 interface Loan {
   id: number;
@@ -16,26 +16,26 @@ interface Loan {
   submission_date: string;
   start_date: string | null;
   end_date: string | null;
-  status: "under_review" | "approved" | "rejected" ;
+  status: "under_review" | "approved" | "rejected";
 }
 
 export default function LoanDashboard() {
   const [loans, setLoans] = useState([]);
-  const t = useTranslations('userProfile.loansPage');
+  const t = useTranslations("userProfile.loansPage");
   const locale = useLocale();
-  
+
   const getLoans = async () => {
     const loansData = await getUserLoansList(1, 10);
     setLoans(loansData?.loans ?? []);
-  }
-  
+  };
+
   useEffect(() => {
     getLoans();
   }, []);
-  
-  console.log(loans);
+
+  console.log(loans, "lllllll");
   const router = useRouter();
-  
+
   return (
     <div className="bg-white rounded-lg p-6 shadow-sm">
       {/* Header */}
@@ -45,13 +45,15 @@ export default function LoanDashboard() {
             <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
               <FaCommentDollar className="w-6 h-6 text-primary" />
             </div>
-            <span className="text-primary text-lg font-bold">{t('title')}</span>
+            <span className="text-primary text-lg font-bold">{t("title")}</span>
           </div>
-          <button 
-            onClick={() => router.push(`/${locale}/user-profile/loans/loan-request`)} 
+          <button
+            onClick={() =>
+              router.push(`/${locale}/user-profile/loans/loan-request`)
+            }
             className="border-2 border-secondary text-secondary px-5 py-1 rounded-full font-bold cursor-pointer hover:bg-secondary hover:text-white transition-colors"
           >
-            {t('requestLoan')}
+            {t("requestLoan")}
           </button>
         </div>
       </div>
@@ -59,17 +61,17 @@ export default function LoanDashboard() {
       {/* Loan Cards */}
       <div className="space-y-4">
         {loans.map((loan: Loan) => (
-          <LoanCard 
-            key={loan.id} 
-            id={loan.id} 
-            name={loan.name} 
-            loan_amount={loan.loan_amount} 
-            loan_reason={loan.loan_reason} 
-            number_of_installments={loan.number_of_installments} 
-            submission_date={loan.submission_date} 
-            start_date={loan.start_date} 
-            end_date={loan.end_date} 
-            status={loan.status} 
+          <LoanCard
+            key={loan.id}
+            id={loan.id}
+            name={loan.name}
+            loan_amount={loan.loan_amount}
+            loan_reason={loan.loan_reason}
+            number_of_installments={loan.number_of_installments}
+            submission_date={loan.submission_date}
+            start_date={loan.start_date}
+            end_date={loan.end_date}
+            status={loan.status}
           />
         ))}
       </div>
